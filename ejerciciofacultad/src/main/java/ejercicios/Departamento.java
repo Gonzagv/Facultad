9d3d531f0d095751b8f369ca5ce42b66d6f1efa7
Gonzagv <gonzig1996@gmail.com>
2019-08-16 11:15:11 -0300

    Paso ejercicio facultad en limpio

---
+++ ejerciciofacultad/src/main/java/ejercicios/Departamento.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class Departamento {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ ejerciciofacultad/src/main/java/ejercicios/EstadoCivilEnum.java
@@ -0,0 +1,6 @@
+package ejercicios;
+
+public enum EstadoCivilEnum {
+
+    SOLTERO, CASADO, DIVORCIADO
+}
